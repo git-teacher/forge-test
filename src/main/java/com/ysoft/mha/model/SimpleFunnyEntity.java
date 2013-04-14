81808f7bb1310761d496bed8e3da1d7b4db1e8f9
Matthew Lowe <matthew.lowe2@hotmail.com>
2013-04-14 19:58:21 +0200

    Initial commit with basic structure

---
+++ src/main/java/com/ysoft/mha/model/SimpleFunnyEntity.java
@@ -0,0 +1,97 @@
+package com.ysoft.mha.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+
+@Entity
+public class SimpleFunnyEntity implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id = null;
+   @Version
+   @Column(name = "version")
+   private int version = 0;
+
+   @Column
+   private String clownName;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object that)
+   {
+      if (this == that)
+      {
+         return true;
+      }
+      if (that == null)
+      {
+         return false;
+      }
+      if (getClass() != that.getClass())
+      {
+         return false;
+      }
+      if (id != null)
+      {
+         return id.equals(((SimpleFunnyEntity) that).id);
+      }
+      return super.equals(that);
+   }
+
+   @Override
+   public int hashCode()
+   {
+      if (id != null)
+      {
+         return id.hashCode();
+      }
+      return super.hashCode();
+   }
+
+   public String getClownName()
+   {
+      return this.clownName;
+   }
+
+   public void setClownName(final String clownName)
+   {
+      this.clownName = clownName;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (clownName != null && !clownName.trim().isEmpty())
+         result += "clownName: " + clownName;
+      return result;
+   }
+}
