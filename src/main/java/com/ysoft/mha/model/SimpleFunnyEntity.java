c782d551b77f0e0b1e2ce45422f8ab68219c884d
Matthew Lowe <matthew.lowe2@hotmail.com>
2013-04-14 21:03:34 +0200

    Mato is new clown

--- src/main/java/com/ysoft/mha/model/SimpleFunnyEntity.java
+++ src/main/java/com/ysoft/mha/model/SimpleFunnyEntity.java
@@ -22,7 +22,7 @@ public class SimpleFunnyEntity implements Serializable
    private int version = 0;
 
    @Column
-   private String clownName;
+   private String clownName = "Mato";
 
    public Long getId()
    {
